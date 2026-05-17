9f5c6601bbde9fe92287758920b946c3d8dc9e5b
Manukumar200424 <kumarmanu6818@gmail.com>
2026-05-17 22:40:51 +0530

    Time: 259 ms (62.62%), Space: 131.2 MB (48.24%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combine(self, n, k):
+
+        result = []
+
+        def backtrack(start, path):
+
+            # If combination size becomes k
+            if len(path) == k:
+                result.append(path[:])
+                return
+
+            # Generate combinations
+            for i in range(start, n + 1):
+                path.append(i)
+                backtrack(i + 1, path)
+                path.pop()
+
+        backtrack(1, [])
+
+        return result

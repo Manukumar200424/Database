c80465b4f595be35f3697544648c58985382c286
Manukumar200424 <kumarmanu6818@gmail.com>
2026-06-12 22:31:52 +0530

    Time: 28 ms (90.96%), Space: 47.7 MB (32.49%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minCut(String s) {
+        int n = s.length();
+
+        boolean[][] isPal = new boolean[n][n];
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = i; // maximum cuts
+        }
+
+        for (int end = 0; end < n; end++) {
+            for (int start = 0; start <= end; start++) {
+
+                if (s.charAt(start) == s.charAt(end) &&
+                    (end - start <= 2 || isPal[start + 1][end - 1])) {
+
+                    isPal[start][end] = true;
+
+                    if (start == 0) {
+                        dp[end] = 0;
+                    } else {
+                        dp[end] = Math.min(dp[end], dp[start - 1] + 1);
+                    }
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+}
